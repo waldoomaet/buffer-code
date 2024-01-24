4bcda5f68f4d59f4c46efbea010a4df9857c5d1e
Waldo Ruiz <waldo.omaet6446@gmail.com>
2024-01-24 14:24:16 +0100

    ver

--- basestation.c
+++ basestation.c
@@ -22,20 +22,31 @@ static int count = 0;
  * As the client does not need to receive, the function does not do anything
  */
 static void recv(const void *data, uint16_t len,
-  const linkaddr_t *src, const linkaddr_t *dest) {
+                 const linkaddr_t *src, const linkaddr_t *dest)
+{
+  count++;
+  /* 0bxxxxx allows us to write binary values */
+  /* for example, 0b10 is 2 */
+  leds_off(LEDS_ALL);
+  leds_on(count & 0b1111);
+  
+  printf("Basestation got something: ");
+  int count = 0;
+  while (count < len)
+  {
+    printf("%d", *(int *)(data + count));
     count++;
-    /* 0bxxxxx allows us to write binary values */
-    /* for example, 0b10 is 2 */
-    leds_off(LEDS_ALL);
-    leds_on(count & 0b1111);
+  }
+  printf(" \n");
 }
 
 /* Our main process. */
-PROCESS_THREAD(basestation_process, ev, data) {
-	PROCESS_BEGIN();
+PROCESS_THREAD(basestation_process, ev, data)
+{
+  PROCESS_BEGIN();
 
-	/* Initialize NullNet */
-	nullnet_set_input_callback(recv);
+  /* Initialize NullNet */
+  nullnet_set_input_callback(recv);
 
-	PROCESS_END();
+  PROCESS_END();
 }
