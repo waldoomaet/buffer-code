d9cf42417f198a7e4c71d59e9e5d8b37b80e1a35
Waldo Ruiz <waldo.omaet6446@gmail.com>
2024-01-25 14:38:28 +0100

    ver

--- basestation.c
+++ basestation.c
@@ -9,7 +9,7 @@
 
 PROCESS(timer_process, "Timer process");
 PROCESS(basestation_process, "Clicker basestation");
-AUTOSTART_PROCESSES(&basestation_process, &timer_process);
+AUTOSTART_PROCESSES(&basestation_process);
 
 static struct etimer et;
 
@@ -27,8 +27,8 @@ static void recv(const void *data, uint16_t len,
     len_count += 2;
   }
   printf(" \n");
-  process_poll(&timer_process);
   etimer_reset(&et);
+  process_poll(&timer_process);
 }
 
 PROCESS_THREAD(basestation_process, ev, data)
@@ -42,13 +42,15 @@ PROCESS_THREAD(basestation_process, ev, data)
 
 PROCESS_THREAD(timer_process, ev, data)
 {
-  etimer_set(&et, STILL_INTERVAL);
+  // etimer_set(&et, STILL_INTERVAL);
   PROCESS_BEGIN();
 
   while (1)
   {
-    PROCESS_YIELD_UNTIL(ev == PROCESS_EVENT_POLL && etimer_expired(&et));
-    printf("Event in timer process!\n");
+    etimer_set(&et, STILL_INTERVAL);
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+    // PROCESS_YIELD_UNTIL(ev == PROCESS_EVENT_POLL);
+    printf("Event in timer process! Time left: \n", );
   }
 
   PROCESS_END();
